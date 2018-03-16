7afbca697b3785d328d9ddb1da11116adaa24c9d
“Amjad <amjad.k@hcl.com>
2018-03-16 16:46:11 +0530

    Implemented Popup View.

--- CustomPopUp/ViewController.swift
+++ CustomPopUp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnPopUp: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showPopUpButtonPressed(_ sender: Any) {
+        
+        let popUpView = CustomPopUp()
+        popUpView.preferredContentSize = CGSize(width: self.view.frame.size.width - 100, height: 249)
+        popUpView.popoverPresentationController?.sourceView = btnPopUp
+        popUpView.popoverPresentationController?.sourceRect = CGRect(x: 0, y: 0, width: btnPopUp.bounds.width, height: btnPopUp.bounds.height + 5) // btnPopUp.bounds
+        popUpView.popoverPresentationController?.backgroundColor = UIColor.red
+        self.present(popUpView, animated: true, completion: nil)
+    }
+    
+}
 
+class CustomPopUp: UIViewController, UIPopoverPresentationControllerDelegate {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        self.modalPresentationStyle = .popover
+        self.popoverPresentationController?.delegate = self
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        return UIModalPresentationStyle.none
+    }
 }
 
